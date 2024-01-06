e7e2a73f0884acc76b97285259aa22a66a06c948
Aminat Adebayo <adebayo.aminat@gmail.com>
2024-01-06 16:20:57 -0500

    projects

---
+++ src/components/Projects.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import {data} from '../projects/Project.jsx'
+
+const Projects = () => {
+    // projects file
+    const project = data;
+
+  return (
+    <div name='projects' className='w-full md:h-screen text-[#1f2435] bg-[#f1ecea]  pt-12 md:pt-16'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+          <p className='text-4xl font-bold inline border-b-4 text-[#1f2435]'>
+            Projects
+          </p>
+          {/* <p className='py-6'>My recent work</p> */}
+        </div>
+
+{/* container for projects */}
+<div className="grid sm:grid-cols-4 md:grid-cols-3 gap-4">
+          
+          {/* Gird Item */}
+          {project.map((item, index) => (
+  <div
+    key={index}
+    style={{ backgroundImage: `url(${item.image})` }}
+    className="shadow-lg shadow-[#040c16] group container rounded-md 
+              flex justify-center text-center items-center mx-auto content-div "
+  >
+    {/* Hover effect for images */}
+    <div className="opacity-0 group-hover:opacity-100 ">
+      <span className="text-xl font-bold text-[#1f2435]] tracking-wider ">
+        {item.name}
+        <br></br>
+        {item.summary}
+        <br></br>
+        Tech Stack: {item.tech}
+      </span>
+      <div className="pt-8 text-center ">
+        <a href={item.github} target="_blank">
+          <button
+            className="text-center rounded-lg px-4 py-3 m-2
+                       bg-[#8892b0] text-gray-700 font-bold text-lg"
+          >
+            Github
+          </button>
+        </a>
+        <a href={item.live} target="_blank">
+          <button
+            className="text-center rounded-lg px-4 py-3 m-2
+                       bg-[#8892b0] text-gray-700 font-bold text-lg"
+          >
+            Walkthough
+          </button>
+        </a>
+      </div>
+    </div>
+  </div>
+))}
+
+
+</div>
+      </div>
+    </div>
+  );
+};
+export default Projects
---
+++ src/projects/Project.jsx
@@ -0,0 +1,43 @@
+import Blog from '../assets/Blog.png';
+import RRNYC from '../assets/RRNYC.jpg';
+import TransXceed from '../assets/TransXceed.jpg';
+
+
+export const data=[
+    {
+        id:1,
+        name:"Restroom Radar NYC",
+        summary: "Your ultimate companion for seamless bathroom hunting across the streets of New York City. ",
+        tech: " Python, Flask, SQLAlchemy, React, HTML/CSS, Redux ",
+        image:RRNYC,
+        github:"https://github.com/Amii911/Restroom-Radar-NYC",
+        live:"https://youtu.be/IGvnnkiywwo",
+    },
+    {
+        id:2,
+        name:"TransXceed",
+        summary: "A Command Line Interface Application designed to streamline investments.",
+        tech: "Python, SQLite, SQLAlchemy, CLI, Alembic",
+        image:TransXceed,
+        github:"https://github.com/Amii911/TransXceed",
+        live:"https://youtu.be/a6B-StFFslU",
+    },
+    {
+        id:3,
+        name:"Bloggin Blog",
+        summary: "A space for users to share their story, speak their mind and even hang onto a memory. ",
+        tech: " JavaScript, React, HTML,  CSS",
+        image:Blog,
+        github:"https://github.com/Amii911/bloggin-blog",
+        live:"https://youtu.be/2LukRw3nzuI",
+    },
+    // {
+    //     id:4,
+    //     name:"React JS Application",
+    //     image:RealEst,
+    //     github:"",
+    //     live:"",
+    // },
+
+
+]
